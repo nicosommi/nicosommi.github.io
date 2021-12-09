0b3714f9d2418957ef0f64866014c8c59c0efa22
nicosommi <nicosommi@gmail.com>
2021-12-09 14:12:39 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7baab1abc718965eeb40.js"
+    "url": "webpack-runtime-ad6915536fcb668b02dc.js"
   },
   {
     "url": "framework-8d714a4faf31edeed8fb.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9879d3fd30f3486265cc1ed98fc27994"
+    "revision": "beb649f9dacb4eab6d8d9173caf0b282"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-48f442ae594c3544ac16.js"
