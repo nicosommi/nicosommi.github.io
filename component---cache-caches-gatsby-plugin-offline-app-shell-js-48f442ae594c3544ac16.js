c08c4f8792f211cc3165b8f322172a57fb299e11
nico <1702755+nicosommi@users.noreply.github.com>
2023-02-12 17:51:26 +0100

    feat: switch blog to vercel for now

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-48f442ae594c3544ac16.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpersonal_blog=self.webpackChunkpersonal_blog||[]).push([[306],{1930:function(e,t,n){var u=n(8580);t.__esModule=!0,t.default=void 0;var l=u(n(3793)),r=u(n(7294)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,l.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-48f442ae594c3544ac16.js.map
